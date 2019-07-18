3886664b38abc5cd92fd14c9a02541b861fcc700
Vita <vitalia@myrambler.ru>
2019-07-18 17:12:20 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -5,15 +5,33 @@ const welcome = document
 const gameGenre = document
   .getElementById(`game-genre`)
   .content.querySelector(`.game`);
-const gameArtist = document.getElementById(`game-artist`);
-const resultSuccess = document.getElementById(`result-success`);
-const failTime = document.getElementById(`fail-time`);
-const failTries = document.getElementById(`fail-tries`);
-const resultArtist = document.getElementById(`result-artist`);
-const resultList = document.getElementById(`result-list`);
-const resultGenre = document.getElementById(`result-genre`);
-const modalError = document.getElementById(`modal-error`);
-const modalConfirm = document.getElementById(`modal-confirm`);
+const gameArtist = document
+  .getElementById(`game-artist`)
+  .content.querySelector(`.game`);
+const resultSuccess = document
+  .getElementById(`result-success`)
+  .content.querySelector(`.result`);
+const failTime = document
+  .getElementById(`fail-time`)
+  .content.querySelector(`.result`);
+const failTries = document
+  .getElementById(`fail-tries`)
+  .content.querySelector(`.result`);
+const resultArtist = document
+  .getElementById(`result-artist`)
+  .content.querySelector(`.result`);
+const resultList = document
+  .getElementById(`result-list`)
+  .content.querySelector(`.result`);
+const resultGenre = document
+  .getElementById(`result-genre`)
+  .content.querySelector(`.result`);
+const modalError = document
+  .getElementById(`modal-error`)
+  .content.querySelector(`.modal`);
+const modalConfirm = document
+  .getElementById(`modal-confirm`)
+  .content.querySelector(`.modal`);
 
 const ready = () => {
   main.appendChild(welcome);
@@ -46,22 +64,55 @@ document.addEventListener("DOMContentLoaded", ready);
 let counter = 0;
 const switcher = () => {
   console.log(event.which);
-  const kek = () => {
-    if (event.which == 39) {
-      counter++;
-      console.log(counter);
-      main.appendChild(gameGenre);
-      main.removeChild(welcome);
-    }
-    if (event.which == 37) {
-      counter--;
-      console.log(counter);
-      main.appendChild(welcome);
-      main.removeChild(gameGenre);
-    }
-  };
-  kek();
+  if (event.which == 39) {
+    counter++;
+    slider(counter);
+  }
+  if (event.which == 37) {
+    counter--;
+    slider(counter);
+  }
   console.log(counter);
+};
+
+const slider = counter => {
+  console.log(counter);
+  switch (counter) {
+    case 0:
+      main.removeChild(gameGenre);
+      main.appendChild(welcome);
+      break;
+    case 1:
+      if (gameArtist.parentNode == main) {
+        main.removeChild(gameArtist);
+      } else {
+        main.removeChild(welcome);
+      }
+      main.appendChild(gameGenre);
+      break;
+    case 2:
+      if (gameGenre.parentNode == main) {
+        main.removeChild(gameGenre);
+      } else {
+        main.removeChild(resultSuccess);
+      }
+      main.appendChild(gameArtist);
+      break;
+    case 3:
+      main.removeChild(gameArtist);
+      main.appendChild(resultSuccess);
+      break;
+    case 4:
+      main.removeChild(resultSuccess);
+      main.appendChild(resultGenre);
+      break;
+    case 5:
+      main.removeChild(resultGenre);
+      main.appendChild(resultList);
+      break;
+    default:
+      break;
+  }
 };
 
 document.addEventListener("keydown", switcher);

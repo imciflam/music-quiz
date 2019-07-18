63f07916712a22327d84f9673ddee6c8527ffaaa
Vita <vitalia@myrambler.ru>
2019-07-18 15:00:10 +0300

    + start page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-const welcome = document.getElementById(`welcome`);
-const gameGenre = document.getElementById(`game-genre`);
+const main = document.querySelector(`.main`);
+const welcome = document
+  .getElementById(`welcome`)
+  .content.querySelector(`.welcome`);
+const gameGenre = document
+  .getElementById(`game-genre`)
+  .content.querySelector(`.game`);
 const gameArtist = document.getElementById(`game-artist`);
 const resultSuccess = document.getElementById(`result-success`);
 const failTime = document.getElementById(`fail-time`);
@@ -9,3 +14,16 @@ const resultList = document.getElementById(`result-list`);
 const resultGenre = document.getElementById(`result-genre`);
 const modalError = document.getElementById(`modal-error`);
 const modalConfirm = document.getElementById(`modal-confirm`);
+
+document.addEventListener("DOMContentLoaded", ready);
+
+function ready() {
+  main.appendChild(welcome);
+}
+
+document.addEventListener("keydown", function(event) {
+  console.log(event.which);
+  if (event.which == 39) {
+    main.appendChild(gameGenre);
+  }
+});

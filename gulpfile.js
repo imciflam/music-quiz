28d1cde2a9af050f551441c96798e8ff1ee93752
Vita <vitalia@myrambler.ru>
2019-07-18 17:24:51 +0300

    + dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const del = require(`del`);
 const gulp = require(`gulp`);
@@ -11,65 +11,74 @@ const mqpacker = require(`css-mqpacker`);
 const minify = require(`gulp-csso`);
 const rename = require(`gulp-rename`);
 const imagemin = require(`gulp-imagemin`);
+const rollup = require(`gulp-better-rollup`);
+const rollup = require(`gulp-better-rollup`);
 
 gulp.task(`style`, () => {
-  return gulp.src(`sass/style.scss`).
-    pipe(plumber()).
-    pipe(sass()).
-    pipe(postcss([
-      autoprefixer({
-        browsers: [
-          `last 1 version`,
-          `last 2 Chrome versions`,
-          `last 2 Firefox versions`,
-          `last 2 Opera versions`,
-          `last 2 Edge versions`
-        ]
-      }),
-      mqpacker({sort: true})
-    ])).
-    pipe(gulp.dest(`build/css`)).
-    pipe(server.stream()).
-    pipe(minify()).
-    pipe(rename(`style.min.css`)).
-    pipe(gulp.dest(`build/css`));
+  return gulp
+    .src(`sass/style.scss`)
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(
+      postcss([
+        autoprefixer({
+          browsers: [
+            `last 1 version`,
+            `last 2 Chrome versions`,
+            `last 2 Firefox versions`,
+            `last 2 Opera versions`,
+            `last 2 Edge versions`
+          ]
+        }),
+        mqpacker({ sort: true })
+      ])
+    )
+    .pipe(gulp.dest(`build/css`))
+    .pipe(server.stream())
+    .pipe(minify())
+    .pipe(rename(`style.min.css`))
+    .pipe(gulp.dest(`build/css`));
 });
 
 gulp.task(`scripts`, () => {
-  return gulp.src(`js/**/*.js`).
-    pipe(plumber()).
-    pipe(gulp.dest(`build/js/`));
+  return gulp
+    .src(`js/**/*.js`)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(rollup({}, `iife`))
+    .pipe(sourcemaps.write(``))
+    .pipe(gulp.dest(`build/js/`));
 });
-
 gulp.task(`imagemin`, [`copy`], () => {
-  return gulp.src(`build/img/**/*.{jpg,png,gif}`).
-    pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true})
-    ])).
-    pipe(gulp.dest(`build/img`));
+  return gulp
+    .src(`build/img/**/*.{jpg,png,gif}`)
+    .pipe(
+      imagemin([
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.jpegtran({ progressive: true })
+      ])
+    )
+    .pipe(gulp.dest(`build/img`));
 });
-
 
 gulp.task(`copy-html`, () => {
-  return gulp.src(`*.{html,ico}`).
-    pipe(gulp.dest(`build`)).
-    pipe(server.stream());
+  return gulp
+    .src(`*.{html,ico}`)
+    .pipe(gulp.dest(`build`))
+    .pipe(server.stream());
 });
 
 gulp.task(`copy`, [`copy-html`, `scripts`, `style`], () => {
-  return gulp.src([
-    `fonts/**/*.{woff,woff2}`,
-    `img/*.*`
-  ], {base: `.`}).
-    pipe(gulp.dest(`build`));
+  return gulp
+    .src([`fonts/**/*.{woff,woff2}`, `img/*.*`], { base: `.` })
+    .pipe(gulp.dest(`build`));
 });
 
 gulp.task(`clean`, () => {
   return del(`build`);
 });
 
-gulp.task(`js-watch`, [`scripts`], (done) => {
+gulp.task(`js-watch`, [`scripts`], done => {
   server.reload();
   done();
 });
@@ -84,7 +93,7 @@ gulp.task(`serve`, [`assemble`], () => {
   });
 
   gulp.watch(`sass/**/*.{scss,sass}`, [`style`]);
-  gulp.watch(`*.html`).on(`change`, (e) => {
+  gulp.watch(`*.html`).on(`change`, e => {
     if (e.type !== `deleted`) {
       gulp.start(`copy-html`);
     }
@@ -100,5 +109,4 @@ gulp.task(`build`, [`assemble`], () => {
   gulp.start(`imagemin`);
 });
 
-gulp.task(`test`, () => {
-});
+gulp.task(`test`, () => {});

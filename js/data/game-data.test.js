7bc36edca14f4d6b8cf01def5928e7fb3d62d463
Vita <vitalia@myrambler.ru>
2019-07-28 13:01:38 +0300

    + counting test

--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -13,6 +13,10 @@ const results = [
   {
     attempt: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     livesLeft: 1
+  },
+  {
+    attempt: [3, 1, 1, 1, 1, 1, 3, 1, 1, 1],
+    livesLeft: 3
   }
 ];
 
@@ -25,11 +29,11 @@ describe(`Результаты игр`, () => {
     assert.equal(10, resultsCount(results[1]));
   });
 
-  it(`Игрок ответил правильно и не быстро на ${QUESTIONS_COUNT} вопросов, ни разу не ошибся`, () => {
-    assert.equal(10, resultsCount(results[1]));
-  });
-
   it(`Игрок ответил на не меньше, чем  ${QUESTIONS_COUNT} вопросов, ошибался`, () => {
     assert.notEqual(10, resultsCount(results[2]));
+  });
+
+  it(`игрок  ответил на ${QUESTIONS_COUNT} вопросов, подсчитать очки`, () => {
+    assert.equal(14, resultsCount(results[3]));
   });
 });
